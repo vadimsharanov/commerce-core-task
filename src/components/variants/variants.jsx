17db173640336e27028a923f59fc3dcbd8d7f987
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-12-15 18:46:44 +0200

    variants changes

--- src/components/variants/variants.jsx
+++ src/components/variants/variants.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import "./variants.scss";
 
+const singleProduct =
+  "https://lh3.googleusercontent.com/Asov37bn3f3dSu086PZ5-keey-rLusH-E9ZcoT-AwlkNumNQM_KX6bWAdKWiQttFe3mmB5g=s40";
+const products =
+  "https://lh3.googleusercontent.com/sq4VFfXzwP3Rorjh7XIaOSifkop-5WHFkx5IbNWBuWajzSYJ45AwnbAGsuYab8-zB93JDw=s40";
 const Variants = () => {
-  return <h1>variants</h1>;
+  return (
+    <div className='variants-container'>
+      <div className='variants-title'>Title</div>
+      <div className='variants-box'>
+        <div className='variants-dropdown'>
+          <div className='variants-dropdown-left-side'>
+            <img src={products} alt='' />
+            <div>1xCoreProduct &reg;</div>
+          </div>
+          <div className='variants-dropdown-right-side'>
+            <span>Change</span>
+            <span>&#8250;</span>
+          </div>
+        </div>
+        <div className='variants-dropdown-content'>
+          <div className='variants-dropdown-content-single'>
+            <div className='variants-dropdown-left-side'>
+              <img src={products} alt='' />
+              <div>1xCoreProduct &reg;</div>
+            </div>
+            <div className='variants-dropdown-right-side'>$19.99</div>
+          </div>
+          <div className='variants-dropdown-content-single'>
+            <div className='variants-dropdown-left-side'>
+              <img src={products} alt='' />
+              <div>1xCoreProduct &reg;</div>
+            </div>
+            <div className='variants-dropdown-right-side'>$19.99</div>
+          </div>
+          <div className='variants-dropdown-content-single'>
+            <div className='variants-dropdown-left-side'>
+              <img src={products} alt='' />
+              <div>1xCoreProduct &reg;</div>
+            </div>
+            <div className='variants-dropdown-right-side'>$19.99</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Variants;
