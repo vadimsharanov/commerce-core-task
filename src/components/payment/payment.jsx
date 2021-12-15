ea6663771b8e30d42684ade4571f61f3d87b6cd1
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-12-15 21:14:01 +0200

    variants some changes added, payment started

--- src/components/payment/payment.jsx
+++ src/components/payment/payment.jsx
@@ -2,6 +2,39 @@ import React from "react";
 import "./payment.scss";
 
 const Payment = () => {
-  return <h1>Payment</h1>;
+  return (
+    <div className='payment-container'>
+      <div className='payment-title'> TITLE</div>
+      <form className='payment-form' action=''>
+        <div className='customer-information'>
+          <div>customer information</div>
+          <div>field marked as </div>
+          <div>
+            <input type='text' />
+            <input type='text' />
+          </div>
+          <div>
+            <input type='text' />
+          </div>
+        </div>
+
+        <div className='shipping-adress'>
+          <div>shipping adress</div>
+          <div>
+            <input type='text' />
+          </div>
+          <div>
+            <input type='text' />
+          </div>
+          <div>
+            <span>dropdown</span>
+            <span>dropdown</span>
+            <input type='text' />
+          </div>
+        </div>
+        <div>payment method is coming .... soon</div>
+      </form>
+    </div>
+  );
 };
 export default Payment;
--- src/components/variants/variants.jsx
+++ src/components/variants/variants.jsx
@@ -1,48 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./variants.scss";
-
 const singleProduct =
   "https://lh3.googleusercontent.com/Asov37bn3f3dSu086PZ5-keey-rLusH-E9ZcoT-AwlkNumNQM_KX6bWAdKWiQttFe3mmB5g=s40";
 const products =
   "https://lh3.googleusercontent.com/sq4VFfXzwP3Rorjh7XIaOSifkop-5WHFkx5IbNWBuWajzSYJ45AwnbAGsuYab8-zB93JDw=s40";
 const Variants = () => {
+  const [open, setOpen] = useState(false);
+
+  const showContent = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className='variants-container'>
       <div className='variants-title'>Variants</div>
       <div className='variants-box'>
         <div className='variants-dropdown'>
           <div className='variants-dropdown-left-side'>
-            <img src={products} alt='' />
-            <div>1xCoreProduct &reg;</div>
+            <img src={products} alt='#' />
+            <div>1xCoreProduct&reg;</div>
           </div>
-          <div className='variants-dropdown-right-side'>
+          <div
+            className='variants-dropdown-right-side change-button'
+            onClick={showContent}>
             <span>Change</span>
-            <div className='change-arrow'>&#8250;</div>
+            <i
+              style={{ transform: open ? "" : "rotate(180deg)" }}
+              className='fa fa-chevron-up change-arrow'
+              aria-hidden='true'></i>
           </div>
         </div>
-        <div className='variants-dropdown-content'>
-          <div className='variants-dropdown-content-single'>
-            <div className='variants-dropdown-left-side'>
-              <img src={products} alt='' />
-              <div>1xCoreProduct &reg;</div>
+        <span className='horizontal-line'></span>
+        {open && (
+          <div className='variants-dropdown-content'>
+            <div className='variants-dropdown-content-single'>
+              <div className='variants-dropdown-left-side'>
+                <img src={singleProduct} alt='#' />
+                <div>
+                  <span style={{ fontWeight: 700 }}>1x </span>
+                  CoreProduct&reg;
+                </div>
+              </div>
+              <div className='variants-dropdown-right-side'>$19.99</div>
             </div>
-            <div className='variants-dropdown-right-side'>$19.99</div>
-          </div>
-          <div className='variants-dropdown-content-single'>
-            <div className='variants-dropdown-left-side'>
-              <img src={products} alt='' />
-              <div>1xCoreProduct &reg;</div>
+            <div className='variants-dropdown-content-single'>
+              <div className='variants-dropdown-left-side'>
+                <img src={singleProduct} alt='#' />
+                <div>
+                  <span style={{ fontWeight: 700 }}>1x </span>
+                  CoreProduct&reg;
+                </div>
+              </div>
+              <div className='variants-dropdown-right-side'>$19.99</div>
             </div>
-            <div className='variants-dropdown-right-side'>$19.99</div>
-          </div>
-          <div className='variants-dropdown-content-single'>
-            <div className='variants-dropdown-left-side'>
-              <img src={products} alt='' />
-              <div>1xCoreProduct &reg;</div>
+            <div className='variants-dropdown-content-single'>
+              <div className='variants-dropdown-left-side'>
+                <img src={singleProduct} alt='#' />
+                <div>
+                  <span style={{ fontWeight: 700 }}>1x </span>
+                  CoreProduct&reg;
+                </div>
+              </div>
+              <div className='variants-dropdown-right-side'>$19.99</div>
             </div>
-            <div className='variants-dropdown-right-side'>$19.99</div>
+            <div className='save-changes-button'>
+              <button>SAVE CHANGES</button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
